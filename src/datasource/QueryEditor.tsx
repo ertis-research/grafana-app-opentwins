75bd4c7d4c522ef1cbcf5ec6db22a8a27a56b986
João Capucho <capucho@jcapucho.com>
2025-07-29 17:06:07 +0100

    chore: Add the thing ID input back
    The Thing ID input was replaced with an hardcoded variable to allow for
    a single variable to change all panels in a dashboard. This however
    broke the Alerts & Explore pages as there is no way to specify variables
    there.
    
    This commit reintroduces the Thing ID input and adds supports for
    templating to both inputs, allowing the input to be configured to use a
    variable value.

--- src/datasource/QueryEditor.tsx
+++ src/datasource/QueryEditor.tsx
@@ -1,20 +1,66 @@
-import React, { ChangeEvent } from 'react';
-import { InlineField, Input } from '@grafana/ui';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { InlineField, Input, Combobox, ComboboxOption } from '@grafana/ui';
 import { QueryEditorProps } from '@grafana/data';
 import { DataSource } from './DataSource';
 import { MyDataSourceOptions, MyQuery } from './types';
+import { emptyContext } from 'utils/context/staticContext';
+import { FetchResponse, getBackendSrv } from '@grafana/runtime';
+import { firstValueFrom } from 'rxjs';
 
 type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
 
 export function QueryEditor({ query, onChange, onRunQuery, datasource }: Props) {
+  const { queryText, thingID } = query;
+  const [ids, setIds] = useState<Array<ComboboxOption>>([]);
+
   const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
     onChange({ ...query, queryText: event.target.value });
   };
 
-  const { queryText } = query;
+  const onThingIDChange = (value: string | undefined) => {
+    onChange({ ...query, thingID: value });
+  };
+
+  useEffect(() => {
+    const fetchThingIds = async () => {
+      let context = { ...emptyContext };
+      context.ditto_extended_endpoint = datasource.getBaseUrl();
+
+      try {
+        const twinsResponse: FetchResponse<any[]> = await firstValueFrom(
+          getBackendSrv().fetch<any[]>({
+            url: datasource.getUrl() + datasource.getRoutePath() + `/${datasource.getPath()}/things?option=size(200)`,
+            method: 'GET',
+          })
+        );
+
+        const twins = twinsResponse.data;
+
+        let res = twins.map((item: any) => ({
+          label: item.thingId, // Displayed text
+          value: item.thingId, // Actual value
+        }));
+        setIds(res);
+      } catch (error) {
+        console.error('Error fetching thing IDs:', error);
+      }
+    };
+
+    fetchThingIds();
+  }, []);
 
   return (
     <>
+      <InlineField label="Thing ID" labelWidth={20} tooltip="Not used yet">
+        <Combobox
+          id="query-editor-thing-id"
+          createCustomValue={true}
+          options={ids}
+          onChange={(option) => onThingIDChange(option.value)}
+          value={thingID || ''}
+          placeholder="Enter a query"
+        />
+      </InlineField>
       <InlineField label="Property Path" labelWidth={20} tooltip="Not used yet">
         <Input
           id="query-editor-query-text"
